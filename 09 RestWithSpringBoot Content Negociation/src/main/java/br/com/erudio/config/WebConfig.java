af4058039560afcca97ef57013bc15295f45bdc2
Andre <cr4web@gmail.com>
2021-10-16 12:52:41 -0300

    aula 9

---
+++ 09 RestWithSpringBoot Content Negociation/src/main/java/br/com/erudio/config/WebConfig.java
@@ -0,0 +1,41 @@
+package br.com.erudio.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer{
+	
+	@SuppressWarnings("deprecation")
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+		// via EXTENSION
+		/*configurer.favorParameter(false)
+			.ignoreAcceptHeader(false)
+			.defaultContentType(MediaType.APPLICATION_JSON)
+			.mediaType("json", MediaType.APPLICATION_JSON)
+			.mediaType("xml", MediaType.APPLICATION_XML);*/
+		
+		// VIA QUERY PARAM
+		/*configurer.favorPathExtension(false)
+		.favorParameter(true)
+		.parameterName("mediaType")
+		.ignoreAcceptHeader(false)
+		.useRegisteredExtensionsOnly(false)
+		.defaultContentType(MediaType.APPLICATION_JSON)
+		.mediaType("json", MediaType.APPLICATION_JSON)
+		.mediaType("xml", MediaType.APPLICATION_XML);*/
+		
+		configurer.favorPathExtension(false)
+		.favorParameter(false)
+		.ignoreAcceptHeader(false)
+		.useRegisteredExtensionsOnly(false)
+		.defaultContentType(MediaType.APPLICATION_JSON)
+		.mediaType("json", MediaType.APPLICATION_JSON)
+		.mediaType("xml", MediaType.APPLICATION_XML);
+	}
+}
